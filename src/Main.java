cbb019bb9999bf15d33d2a0582654a9f59f29183
YOUNGJI KIM <yjkim7885@knou.ac.kr>
2025-07-11 08:22:49 +0900

    산술 연산자

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello world!!");
+
+        //Java 연산자
+        //산술 연산자: + (더하기),  (빼기),  (곱하기), / (나누기), % (나머지)\
+        int a = 10;
+        int b = 3;
+        System.out.println(a + b); // 13
+        System.out.println(a % b); // 1
+
+        // first commit
+        //System.out.printf("Hello world!!");
 
     }
 }
