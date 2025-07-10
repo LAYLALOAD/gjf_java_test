abb86be6974d1d30c270f56a656ed3f8eb268af2
YOUNGJI KIM <yjkim7885@knou.ac.kr>
2025-07-11 08:28:11 +0900

    대입 연산자

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        //Java 연산자
-        //산술 연산자: + (더하기),  (빼기),  (곱하기), / (나누기), % (나머지)\
-        int a = 10;
-        int b = 3;
-        System.out.println(a + b); // 13
-        System.out.println(a % b); // 1
+        //대입 연산자: =, +=, =, =, /=, %= 등
+        int x = 5;
+        x += 3; // x = x + 3 → 8
+        System.out.println(x);
 
-        // first commit
-        //System.out.printf("Hello world!!");
+//        //Java 연산자
+//        //산술 연산자: + (더하기),  (빼기),  (곱하기), / (나누기), % (나머지)\
+//        int a = 10;
+//        int b = 3;
+//        System.out.println(a + b); // 13
+//        System.out.println(a % b); // 1
+//
+//        // first commit
+//        //System.out.printf("Hello world!!");
 
     }
 }
