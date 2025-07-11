29a027998152f1465bfc7c89b35d48c7dd2342f4
YOUNGJI KIM <yjkim7885@knou.ac.kr>
2025-07-11 15:01:22 +0900

    인터페이스 예제

---
+++ src/Bird.java
@@ -0,0 +1,11 @@
+public class Bird implements Flyable{
+    @Override
+    public void fly() {
+        System.out.println("새가 날아갑니다.");
+    }
+
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+        bird.fly();
+    }
+}
