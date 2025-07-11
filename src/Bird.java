b6e5c5ae0bc9bcf159bbe4e2e65abbc2693a405d
YOUNGJI KIM <yjkim7885@knou.ac.kr>
2025-07-11 15:05:08 +0900

    다중 인터페이스 예제

--- src/Bird.java
+++ src/Bird.java
@@ -1,11 +1,18 @@
-public class Bird implements Flyable{
+public class Bird implements Flyable, Walkable{
     @Override
     public void fly() {
         System.out.println("새가 날아갑니다.");
     }
 
     public static void main(String[] args) {
-        Bird bird = new Bird();
-        bird.fly();
+        Bird myBird = new Bird();
+        myBird.fly();
+        myBird.walk();
+
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("새가 걷습니다.");
     }
 }
